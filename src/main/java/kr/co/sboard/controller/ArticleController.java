2af6a00d4234a3c9c281833a2e546b9bafcfbbf3
chhak0503 <chhak0503@gmail.com>
2024-03-29 13:20:01 +0900

    sboard deploy 1

---
+++ src/main/java/kr/co/sboard/controller/ArticleController.java
@@ -0,0 +1,99 @@
+package kr.co.sboard.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import kr.co.sboard.config.AppInfo;
+import kr.co.sboard.dto.ArticleDTO;
+import kr.co.sboard.dto.PageRequestDTO;
+import kr.co.sboard.dto.PageResponseDTO;
+import kr.co.sboard.entity.Config;
+import kr.co.sboard.repository.ConfigRepository;
+import kr.co.sboard.service.ArticleService;
+import kr.co.sboard.service.FileService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class ArticleController {
+
+    private final ArticleService articleService;
+    private final ConfigRepository configRepository;
+    /*
+        @ModelAttribute("cate")
+         - modelAttribute("cate", cate)와 동일
+    */
+    @GetMapping("/article/list")
+    public String list(Model model, String cate, PageRequestDTO pageRequestDTO){
+
+        PageResponseDTO pageResponseDTO = null;
+
+        if(pageRequestDTO.getKeyword() == null) {
+            // 일반 글 목록 조회
+            pageResponseDTO = articleService.selectArticles(pageRequestDTO);
+        }else {
+            // 검색 글 목록 조회
+            pageResponseDTO = articleService.searchArticles(pageRequestDTO);
+        }
+
+        log.info("pageResponseDTO : " + pageResponseDTO);
+
+        model.addAttribute(pageResponseDTO);
+
+        return "/article/list";
+    }
+
+    @GetMapping("/article/write")
+    public String write(Model model, String cate, PageRequestDTO pageRequestDTO){
+
+        PageResponseDTO pageResponseDTO = PageResponseDTO.builder()
+                .pageRequestDTO(pageRequestDTO)
+                .build();
+
+        model.addAttribute(pageResponseDTO);
+
+        return "/article/write";
+    }
+
+
+
+    @GetMapping("/article/view")
+    public String view(Model model, String cate, int no){
+        ArticleDTO articleDTO = articleService.findById(no);
+
+        model.addAttribute(articleDTO);
+
+        return "/article/view";
+    }
+
+
+    @PostMapping("/article/write")
+    public String write(HttpServletRequest req, ArticleDTO articleDTO){
+        /*
+            글작성을 테스트할 때는 로그인해야하기 때문에
+            SecurityConfig 인가 설정 수정할 것
+        */
+        String regip = req.getRemoteAddr();
+        articleDTO.setRegip(regip);
+
+        log.info(articleDTO.toString());
+
+        articleService.insertArticle(articleDTO);
+
+        return "redirect:/article/list";
+    }
+
+
+
+    // fileDownload 메서드 FileController로 이동
+
+
+}
---
+++ src/main/java/kr/co/sboard/entity/Config.java
@@ -0,0 +1,45 @@
+package kr.co.sboard.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "config")
+public class Config {
+    /*
+        날짜 : 2024/03/19
+        이름 : 김철학
+        내용 : 게시판 테이블 설정 정보
+
+        cate: 게시판 카테고리
+        boardName : 게시판 이름
+        admin : 게시판 관리자
+        total : 게시판 총 글 등록수
+        createDate : 게시판 생성일
+    */
+
+    @Id
+    private String cate;
+    private String boardName;
+    private String admin;
+
+    @ColumnDefault("0")
+    private int total;
+
+    @CreationTimestamp
+    private LocalDateTime createDate;
+
+
+
+}
